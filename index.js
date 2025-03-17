665df3e50b5eddbeac34cec195059ba237f8ad89
simion samuel <samuel.simion@student.moringaschool.com>
2025-03-17 15:49:20 +0300

    Display Ramen Images

---
+++ index.js
@@ -0,0 +1,54 @@
+const ramens = [
+    { id: 1, name: "Shoyu Ramen", restaurant: "Ichiran", image: "images/shoyu.jpg", rating: 5, comment: "Delicious!" },
+    { id: 2, name: "Miso Ramen", restaurant: "Menya", image: "images/nirvana.jpg", rating: 4, comment: "Very flavorful!" },
+    { id: 3, name: "Tonkotsu Ramen", restaurant: "Ramen-ya", image: "images/naruto.jpg", rating: 3, comment: "Rich broth!" },
+    { id: 4, name: "Gyukotsu Ramen", restaurant: "Kojiro", image: "images/kojiro.jpg", rating: 4, comment: "Amazing taste!" }
+];
+
+let selectedRamen = null;
+
+
+
+
+
+
+//<!--ABCDEFGHIJKLMNOPQRSTUVWXYZ-->
+
+function displayRamens(){
+    const menu = document.getElementById("ramen-menu");
+    menu.innerHTML = "";
+
+    ramens.forEach(ramen =>{
+        const img = document.createElement("img");
+        img.src = ramen.image;
+        img.alt = ramen.name;
+        img.addEventListener("click", ()=> handleClick(ramen));
+        menu.appendChild(img);
+
+    });   
+
+    if (ramens.length > 0) {
+        handleClick(ramens[0]);
+    }
+
+}
+
+function handleClick(ramen){
+    selectedRamen = ramen;
+    document.getElementById("ramen-name").textContent = ramen.name;
+    document.getElementById("ramen-restaurant").textContent = ramen.restaurant;
+    document.getElementById("ramen-image").src = ramen.image;
+    document.getElementById("ramen-rating").textContent = ramen.rating;
+    document.getElementsByClassName("ramen-comment").textContent =ramen.comment;
+}
+
+
+
+// Initialize the app
+function main() {
+    displayRamens();
+    
+}
+
+document.addEventListener("DOMContentLoaded", main);
+
